50885d92e13650aee1a3e3e718b1f122ee9b1372
Andrew Festa <festaa@oregonstate.edu>
2023-08-13 19:34:50 -0700

    added params for controlling strength of mutations

--- scripts/run_ccea.py
+++ scripts/run_ccea.py
@@ -18,7 +18,7 @@ from island_influence.learn.optimizer.cceaV2 import ccea
 from island_influence.setup_env import rand_ring_env, create_agent_policy
 
 
-def test_base_ccea(env, num_sims, num_gens, policy_funcs, exp_dir, base_pop_size=15):
+def run_ccea(env, num_sims, num_gens, policy_funcs, exp_dir, base_pop_size=15, mutation_scalar=0.1, prob_to_mutate=0.05, fitness_update_eps=0):
     env.normalize_rewards = True
     print(f'=' * 80)
     print('Testing base ccea')
@@ -44,8 +44,8 @@ def test_base_ccea(env, num_sims, num_gens, policy_funcs, exp_dir, base_pop_size
     opt_start = time.process_time()
     trained_pops, top_inds, gens_run = ccea(
         env, agent_policies=agent_pops, population_sizes=population_sizes,
-        max_iters=num_gens, num_sims=num_sims, experiment_dir=exp_dir, track_progress=True, use_mp=False,
-        mutation_scalar=0.1, prob_to_mutate=0.05, fitness_update_eps=0
+        max_iters=num_gens, num_sims=num_sims, experiment_dir=exp_dir, track_progress=True, use_mp=True,
+        mutation_scalar=mutation_scalar, prob_to_mutate=prob_to_mutate, fitness_update_eps=fitness_update_eps
     )
     opt_end = time.process_time()
     writer.close()
@@ -82,7 +82,7 @@ def main(main_args):
 
     base_ccea_dir = Path(experiment_dir, 'base_ccea')
     for idx in range(num_runs):
-        test_base_ccea(env_func(), num_sims, num_gens, policy_funcs, base_pop_size=base_pop_size, exp_dir=Path(base_ccea_dir, f'stat_run_{idx}'))
+        run_ccea(env_func(), num_sims, num_gens, policy_funcs, base_pop_size=base_pop_size, exp_dir=Path(base_ccea_dir, f'stat_run_{idx}'))
     return
 
 
